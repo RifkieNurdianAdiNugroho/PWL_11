f238ec47103af369e618adf99d6d978565a0b5da
Rifkie <rifkiroar@gmail.com>
2021-04-20 16:25:25 +0700

    Prak1 mengisi auth controller,loginrequest,registerrequest,apirequest dan route

---
+++ app/Http/Requests/ApiRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Response;
+use App\Traits\ApiResponse;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use App\Http\Requests\Validator;
+
+abstract class ApiRequest extends FormRequest
+{
+    use ApiResponse;
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    abstract public function rules();
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException($this->apiError(
+            $validator->errors(),
+            Response::HTTP_UNPROCESSABLE_ENTITY,
+        ));
+    }
+
+
+    public function failedAuthorization()
+    {
+        throw new HttpResponseException($this->apiError(
+            null,
+            Response::HTTP_UNAUTHORIZED
+        ));
+    }
+}
